3b7ef70b0a5de1d9e61ffafdd91a00dd3b1b94ef
Sam Englander <sizeoverload@gmail.com>
2016-07-20 23:01:37 -0400

    can now specify number of layers to make. controlled by outer-loop

--- SamAI_ColorFlagsBtns.jsx
+++ SamAI_ColorFlagsBtns.jsx
@@ -36,68 +36,72 @@ if(app.selection[0] != null) {
 }
 
 var dupNumber = 2, // set number of duplicates
-    myDuplicate,
-    i = 0;
+    myDuplicate;
 
 var myDoc = app.activeDocument;
+
+var thisLayer = myDoc.activeLayer;
+var layerNames = ["1", "2"];
+var itemNames = ["front", "back"];
+
 var selectedArt = null;
 
-var thisLayer = myDoc.activeLayer,
-    itemNames = ["front", "back"];
-
-if(selectedArt == null) {
-    selectedArt = myDoc.selection[0];
-    selectedArt.name = selectedArt.textRange.contents;
-} else {
-    selectedArt.selected = true;
-}
+for(var i = 0; i < 2; i++) {
+    if(selectedArt == null) {
+        selectedArt = myDoc.selection[0];
+        selectedArt.name = selectedArt.textRange.contents;
+    } else {
+        myDoc.selection = null;
+        selectedArt.selected = true;
+    }
 	
-for(var i = 0; i < dupNumber; i++) {
-		myDuplicate = selectedArt.duplicate();
-		//myDuplicate.position = [selectedArt.position[0] + horOffset * (i + 1), selectedArt.position[1] + verOffset * (i + 1)];
-}
+    for(var j = 0; j < itemNames.length; j++) {
+	    myDuplicate = selectedArt.duplicate();
+        //myDuplicate.position = [selectedArt.position[0] + horOffset * (i + 1), selectedArt.position[1] + verOffset * (i + 1)];
+    }
 
-//selectedArt.hidden = true;
-selectedArt.selected = false;
+    //selectedArt.hidden = true;
+    selectedArt.selected = false;
 
-var itemsToStyle = myDoc.selection;
+    var itemsToStyle = myDoc.selection;
 
-for(var i = 0; i < itemsToStyle.length; i++) {
+    for(var j = 0; j < itemsToStyle.length; j++) {
+        myDoc.selection = null;
+        var textOutline = itemsToStyle[j].createOutline(); // this is a group  
+        var compoundPaths = textOutline.compoundPathItems; // each letter is a compound path  
+
+        textOutline.selected = true;
+        textOutline.name = itemNames[j];
+        app.executeMenuCommand("compoundPath"); // merge to single compound path
+    }
+
+    var items = [];
+    
+    for(var j = 0; j < itemNames.length; j++) {
+        items[j] = thisLayer.pageItems.getByName(itemNames[j]);
+    }
+
     myDoc.selection = null;
-    var textOutline = itemsToStyle[i].createOutline(); // this is a group  
-    var compoundPaths = textOutline.compoundPathItems; // each letter is a compound path  
+    var XMLOffset = "<LiveEffect name='Adobe Offset Path'><Dict data='R mlim 4 R ofst 3 I jntp 2 '/></LiveEffect>";
+    items[1].applyEffect(XMLOffset);
+    items[1].selected = true;
+    app.executeMenuCommand("expandStyle");
 
-    textOutline.selected = true;
-    textOutline.name = itemNames[i];
-    app.executeMenuCommand("compoundPath"); // merge to single compound path
-}
+    myDoc.selection = null;
+    items[0].selected = true;
+    app.executeMenuCommand("expandStyle");
 
-var items = [];
-for(var i = 0; i < itemNames.length; i++) {
-    items[i] = thisLayer.pageItems.getByName(itemNames[i]);
-    //alert(items[i].name);
-}
+    var frontLayer = myDoc.layers.add();
+    frontLayer.name = "Layer" + layerNames[i];
 
-myDoc.selection = null;
-var XMLOffset = "<LiveEffect name='Adobe Offset Path'><Dict data='R mlim 4 R ofst 3 I jntp 2 '/></LiveEffect>";
-items[1].applyEffect(XMLOffset);
-items[1].selected = true;
-app.executeMenuCommand("expandStyle");
+    var frontGroup = myDoc.groupItems.add();
+    frontGroup.name = "group";
 
-myDoc.selection = null;
-items[0].selected = true;
-app.executeMenuCommand("expandStyle");
-
-
-var frontLayer = myDoc.layers.add();
-frontLayer.name = "FrontLayer";
-var frontGroup = myDoc.groupItems.add();
-frontGroup.name = "FrontGroup";
-for(var i = 0; i < items.length; i++) {
-    var item = thisLayer.pageItems.getByName(itemNames[i]);
-    var style = app.activeDocument.graphicStyles.getByName(itemNames[i]);
-    style.applyTo(item);
-    item.move(frontGroup, ElementPlacement.PLACEATEND);
+    for(var j = 0; j < items.length; j++) {
+        var item = thisLayer.pageItems.getByName(itemNames[j]);
+        var style = app.activeDocument.graphicStyles.getByName(itemNames[j]);
+        style.applyTo(item);
+        item.move(frontGroup, ElementPlacement.PLACEATEND);
         /*
         var visBounds = item.visibleBounds;
         var x = visBounds[0];
@@ -109,87 +113,11 @@ for(var i = 0; i < items.length; i++) {
         artboard = myDoc.artboards.add(newRect(x,-y,width,height));
         artboard.name = "new name";
         */
+    }
 }
 
-/* 2nd loop */
+/*****
 
-if(selectedArt == null) {
-    selectedArt = myDoc.selection[0];
-    selectedArt.name = selectedArt.textRange.contents;
-} else {
-    myDoc.selection = null;     
-    selectedArt.selected = true;
-}
-	
-for(var i = 0; i < dupNumber; i++) {
-		myDuplicate = selectedArt.duplicate();
-		//myDuplicate.position = [selectedArt.position[0] + horOffset * (i + 1), selectedArt.position[1] + verOffset * (i + 1)];
-}
-
-//selectedArt.hidden = true;
-selectedArt.selected = false;
-
-var itemsToStyle = myDoc.selection;
-
-for(var i = 0; i < itemsToStyle.length; i++) {
-    myDoc.selection = null;
-    var textOutline = itemsToStyle[i].createOutline(); // this is a group  
-    var compoundPaths = textOutline.compoundPathItems; // each letter is a compound path  
-
-    textOutline.selected = true;
-    textOutline.name = itemNames[i];
-    app.executeMenuCommand("compoundPath"); // merge to single compound path
-}
-
-var items = [];
-for(var i = 0; i < itemNames.length; i++) {
-    items[i] = thisLayer.pageItems.getByName(itemNames[i]);
-    //alert(items[i].name);
-}
-
-myDoc.selection = null;
-var XMLOffset = "<LiveEffect name='Adobe Offset Path'><Dict data='R mlim 4 R ofst 3 I jntp 2 '/></LiveEffect>";
-items[1].applyEffect(XMLOffset);
-items[1].selected = true;
-app.executeMenuCommand("expandStyle");
-
-myDoc.selection = null;
-items[0].selected = true;
-app.executeMenuCommand("expandStyle");
-
-
-var frontLayer = myDoc.layers.add();
-frontLayer.name = "FrontLayer";
-var frontGroup = myDoc.groupItems.add();
-frontGroup.name = "FrontGroup";
-for(var i = 0; i < items.length; i++) {
-    var item = thisLayer.pageItems.getByName(itemNames[i]);
-    var style = app.activeDocument.graphicStyles.getByName(itemNames[i]);
-    style.applyTo(item);
-    item.move(frontGroup, ElementPlacement.PLACEATEND);
-        //DON'T NEED THIS TWICE!!!
-        //var visBounds = item.visibleBounds;
-        //var x = visBounds[0];
-        //var y = visBounds[1];
-        //var width = Math.abs(x - visBounds[2]);
-        //var height = Math.abs(y - visBounds[3]);
-        //alert(width + ", " + height);
-        //alert(visBounds);
-        //artboard = myDoc.artboards.add(newRect(x,-y,width,height));
-        //artboard.name = "new name";
-}
-
-
-
-
-
-
-/****************
- *
- *
- ***************/
-
-/*
 var textCompoundPath = app.activeDocument.activeLayer.compoundPathItems.add();
 for (i=0; i<compoundPaths.length; i++) {    // loop thru all letters  
     alert("a");
@@ -205,7 +133,8 @@ for (i=0; i<compoundPaths.length; i++) {    // loop thru all letters
         //pathItem.evenodd=true;
     }  
 }
-*/
 
 //pathItem.fillColor = docActive.swatches[5].color;       // fill it with the 5th swatch  
 //pathItem.strokeColor = docActive.swatches[4].color; // stroke it with the 4th swatch  
+
+******/
